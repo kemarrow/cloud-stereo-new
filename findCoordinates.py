d95db39153709a1747ba33e8842907e308fb0951
kemarrow <katherine.marrow@gmail.com>
2022-02-14 09:58:37 +0000

    check rectification with star points

--- findCoordinates.py
+++ findCoordinates.py
@@ -7,14 +7,16 @@ Created on Fri Oct 29 14:10:39 2021
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-fpR = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C1_20211003_12\C1_031021_frame_146.jpg"
+# fpR = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C1_20211003_12\C1_031021_frame_146.jpg"
+fpR = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\longExposures\tl_2021-10-30_230002_CAL1.jpg"
 imgRLarge = cv.imread(fpR) # camera 1 image
 imgR = cv.resize(imgRLarge,(640,480),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
 
-fpL = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C3_20211003_12\C3_031021_frame_146.jpg"
-# imgLLarge = cv.imread(fpL) # camera 3 image
-# imgL = cv.resize(imgLLarge,(640,480),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
-imgL = cv.imread(fpL)
+#fpL = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C3_20211003_12\C3_031021_frame_146.jpg"
+fpL = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\longExposures\tl4_2021-10-30_230002_CAL1.jpg"
+imgLLarge = cv.imread(fpL) # camera 3 image
+imgL = cv.resize(imgLLarge,(640,480),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
+# imgL = cv.imread(fpL)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -24,33 +26,32 @@ fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
 ax2.imshow(imgR)
 
-
-# Simple mouse click function to store coordinates
-def onclick_lidar(event):
+def onclick_L(event):
     global ix, iy
     ix, iy = event.xdata, event.ydata
 
-    coords_lidar.append((ix, iy))
-
-    # Disconnect after 30 clicks
-    if len(coords_lidar) == 10:
-        fig2.canvas.mpl_disconnect(cid_lidar)
-        plt.close()
-    return
-
-def onclick_im(event):
-    global ix, iy
-    ix, iy = event.xdata, event.ydata
-
-    coords_im.append((ix, iy))
+    coords_L.append((ix, iy))
     
       # Disconnect after 50 clicks
-    if len(coords_im) == 10:
-        fig.canvas.mpl_disconnect(cid_im)
+    if len(coords_L) == 10:
+        fig.canvas.mpl_disconnect(cid_L)
         plt.close()
     return
 
-coords_im = []
-coords_lidar = []
-cid_im = fig.canvas.mpl_connect('button_press_event', onclick_im)
-cid_lidar = fig2.canvas.mpl_connect('button_press_event', onclick_lidar)
+# Simple mouse click function to store coordinates
+def onclick_R(event):
+    global ix, iy
+    ix, iy = event.xdata, event.ydata
+
+    coords_R.append((ix, iy))
+
+    # Disconnect after 30 clicks
+    if len(coords_R) == 10:
+        fig2.canvas.mpl_disconnect(cid_R)
+        plt.close()
+    return
+
+coords_L = []
+coords_R = []
+cid_L = fig.canvas.mpl_connect('button_press_event', onclick_L)
+cid_R = fig2.canvas.mpl_connect('button_press_event', onclick_R)
--- fundamental_estimate_manual.py
+++ fundamental_estimate_manual.py
@@ -83,39 +83,84 @@ CamM_left = np.array([[5.520688775958645920e+02,0.000000000000000000e+00,3.22586
           [0.000000000000000000e+00,5.502640890663026312e+02,2.362389385357402034e+02],
           [0.000000000000000000e+00,0.000000000000000000e+00,1.000000000000000000e+00]])
 
-Distort_left = np.array([2.808374038768443048e-01,-9.909134707088265159e-01,6.299531255281858727e-04,-1.301770463801651002e-03,1.093982545460403522e+00])
+distort = np.array([2.808374038768443048e-01,-9.909134707088265159e-01,6.299531255281858727e-04,-1.301770463801651002e-03,1.093982545460403522e+00])
 
 translation_vector = np.array([ 61.09920255, -16.,   0.])
 
 w,h = 640,480
+    
+starsL = np.array([(632.8054393305439, 267.8347280334728),
+ (631.4665271966527, 281.89330543933056),
+ (628.7887029288703, 295.95188284518827),
+ (589.9602510460251, 208.2531380753138),
+ (524.3535564853556, 254.4456066945607),
+ (597.9937238493724, 40.219665271966505),
+ (618.7468619246862, 25.4916317991632),
+ (454.73012552301253, 137.96025104602512),
+ (417.91004184100416, 71.68410041841008),
+ (391.8012552301255, 262.47907949790795),
+ (389.1234309623431, 189.50836820083686),
+ (377.07322175732213, 201.55857740585776),
+ (330.2112970711297, 214.94769874476987),
+ (312.1359832635983, 73.02301255230128),
+ (254.56276150627613, 18.127615062761492),
+ (219.75104602510459, 272.52092050209205),
+ (215.73430962343096, 232.35355648535568),
+ (330.2112970711297, 214.94769874476987)])
+
+starsR = np.array([(638.1610878661088, 278.5460251046025),
+ (637.4916317991632, 292.60460251046027),
+ (634.8138075313807, 306.663179916318),
+ (592.6380753138075, 217.62552301255232),
+ (527.7008368200836, 265.826359832636),
+ (593.9769874476988, 50.26150627615061),
+ (614.0606694560669, 36.87238493723851),
+ (452.72175732217573, 149.34100418410043),
+ (414.56276150627616, 85.07322175732219),
+ (393.14016736401675, 275.1987447698745),
+ (388.4539748953975, 202.89748953974896),
+ (375.73430962343093, 214.27824267782427),
+ (330.2112970711297, 227.66736401673643),
+ (308.7887029288703, 87.75104602510464),
+ (251.8849372384937, 35.53347280334731),
+ (218.4121338912134, 287.2489539748954),
+ (214.39539748953973, 247.081589958159),
+ (329.5418410041841, 226.99790794979083)])
+
+#rotation,___ = cv2.findHomography(starsR, starsL)
+starsL_3d = np.append(starsL, np.ones((len(starsL), 1)), -1)
+starsR_3d = np.append(starsR, np.ones((len(starsR), 1)), -1)
+rotation, ___ = R.align_vectors(starsR_3d, starsL_3d)
+rotation = rotation.as_matrix()
 
 newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
-    CamM_left, Distort_left, (w, h), 1, (w, h))
-mapx, mapy = cv2.initUndistortRectifyMap(
-    CamM_left, Distort_left, None, newcameramtx, (w, h), 5)
+    CamM_left, distort, (w, h), 1, (w, h))
 
-fundamental_matrix, inlier_mask = cv2.findFundamentalMat(ptsL, ptsR, cv2.FM_RANSAC)
+fundamental_matrix, inlier_mask = cv2.findFundamentalMat(ptsL, ptsR, cv2.FM_RANSAC, 3)
 essential_matrix, inlier_mask = cv2.findEssentialMat(ptsL, ptsR, cameraMatrix=newcameramtx)
 
 relDirs = np.rad2deg(cv2.Rodrigues(cv2.decomposeEssentialMat(essential_matrix)[0])[0])
 
-pose = cv2.recoverPose(essential_matrix, ptsL , ptsR, newcameramtx)
+pose = cv2.recoverPose(essential_matrix, ptsL, ptsR, newcameramtx)
 relDirs2 = np.rad2deg(cv2.Rodrigues(pose[1])[0])
 
 inter_cam_dist = np.sqrt(np.sum(translation_vector**2))
+T = pose[2]*inter_cam_dist
 
 Rleft, Rright, Pleft, Pright, Q, roi_left, roi_right = cv2.stereoRectify(
-    newcameramtx, None,
-    newcameramtx, None,
+    newcameramtx, distort,
+    newcameramtx, distort,
     imageSize=(w,h),
     R=pose[1],
-    T=pose[2]*inter_cam_dist)
+    T=pose[2]*inter_cam_dist,
+    flags= cv2.CALIB_ZERO_DISPARITY)
 
 success, Hleft, Hright = cv2.stereoRectifyUncalibrated(
     np.float32(ptsL), np.float32(ptsR),
     fundamental_matrix, imgSize=(w, h)
     )
-T = pose[2]*inter_cam_dist
+
+
 stereo_cal = {'F': fundamental_matrix,
               'E': essential_matrix,
               'R': pose[1],
@@ -132,4 +177,39 @@ stereo_cal = {'F': fundamental_matrix,
 
 with open('stereo_cal_mat.pkl', 'wb') as f:
     pickle.dump(stereo_cal, f)
+    
+    
+    
+#%%
 
+folder = "C:/Users/kathe/OneDrive - Imperial College London/MSci Project/longExposures/"
+imgRLarge = cv2.imread(folder+'tl_2021-10-30_230002_CAL1.jpg')
+imgLLarge = cv2.imread(folder+'tl4_2021-10-30_230002_CAL1.jpg')
+imgR = cv2.resize(imgRLarge,(640,480),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+imgL = cv2.resize(imgLLarge,(640,480),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, sharex=True, sharey=True)
+ax1.imshow(imgL)
+ax2.imshow(imgR)
+ax1.plot(starsL[:,0], starsL[:,1], 'r.', label='C3 star positions')
+ax2.plot(starsR[:,0], starsR[:,1], 'g.', label='C1 star positions')
+
+
+starsL_rect = cv2.undistortPoints(starsL, newcameramtx, distort, 
+                                  Rleft, np.delete(Pleft, 3, axis=1))
+starsR_rect = cv2.undistortPoints(starsR, newcameramtx, distort, 
+                                  Rright, np.delete(Pright, 3, axis=1))
+
+starsL_rect = starsL_rect.reshape((18,2))
+starsR_rect = starsR_rect.reshape((18,2))
+
+ax3.plot(starsL[:,0], starsL[:,1], 'r.', label='C3 star positions')
+ax3.plot(starsR[:,0], starsR[:,1], 'g.', label='C1 star positions')
+ax3.legend()
+
+
+ax4.plot(starsL[:,0], starsL[:,1], 'rx', label='C3 star positions')
+ax4.plot(starsR[:,0], starsR[:,1], 'gx', label='C1 star positions')
+ax4.plot(starsL_rect[:,0], starsL_rect[:,1], 'r.', label='C3 rectified star positions')
+ax4.plot(starsR_rect[:,0], starsR_rect[:,1], 'g.', label='C1 rectified star positions')
+ax4.legend()
