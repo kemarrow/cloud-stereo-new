f3bfd7e40d8da76e0031ee15f92cd0dc2367c0d5
kemarrow <katherine.marrow@gmail.com>
2022-02-16 12:15:28 +0000

    with Ronnie's matrices

--- findCoordinates.py
+++ findCoordinates.py
@@ -7,16 +7,18 @@ Created on Fri Oct 29 14:10:39 2021
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-# fpR = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C1_20211003_12\C1_031021_frame_146.jpg"
-fpR = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\longExposures\tl_2021-10-30_230002_CAL1.jpg"
+# fpR = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C1_20211003_12\C1_031021_frame_209.jpg"
+fpR = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\longExposures\tl_2021-09-23_230002_CAL1_sharpened.jpg"
 imgRLarge = cv.imread(fpR) # camera 1 image
 imgR = cv.resize(imgRLarge,(640,480),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
+imgR = cv.cvtColor(imgR, cv.COLOR_BGR2RGB)
 
-#fpL = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C3_20211003_12\C3_031021_frame_146.jpg"
-fpL = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\longExposures\tl4_2021-10-30_230002_CAL1.jpg"
+# fpL = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\output_C3_20211003_12\C3_031021_frame_209.jpg"
+fpL = r"C:\Users\kathe\OneDrive - Imperial College London\MSci Project\longExposures\tl4_2021-09-23_230002_CAL1.jpg"
 imgLLarge = cv.imread(fpL) # camera 3 image
 imgL = cv.resize(imgLLarge,(640,480),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
 # imgL = cv.imread(fpL)
+imgL = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
